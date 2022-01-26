1e5ac5f2309e795cb90c75996b7954cdf8d9dd4a
tecarts <axibal.lloyd@gmail.com>
2022-01-25 16:42:01 -0800

    git commit -m "update"

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+window.prompt(weeklyExpenseQuestions[0])
+let answer = window.prompt(weeklyExpenseQuestions[0])
+console.log(answer)
