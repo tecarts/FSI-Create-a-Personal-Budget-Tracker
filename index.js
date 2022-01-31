6b14766c8232d9cc4e785c86d839c04480e8eda2
tecarts <axibal.lloyd@gmail.com>
2022-01-31 01:09:33 -0800

    added monthly and annual expenses

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-window.prompt(weeklyExpenseQuestions[0])
-let answer = window.prompt(weeklyExpenseQuestions[0])
-console.log(answer)
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+
+for(let i = 0; i < monthlyExpenseQuestions.length; i++) 
+{
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + answer
+}
+
+for(let i = 0; i < annualExpenseQuestions.length; i++) 
+{
+    let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + answer
+}
